31048e8dd79c73564d0c050cf6b8be8bdc795c8e
Aman Lohar <loharaman44@gmail.com>
2026-07-07 19:38:38 +0530

    Fix Railway PORT

--- backend/server.js
+++ backend/server.js
@@ -100,6 +100,8 @@ app.post("/verify-payment", (req, res) => {
 
 // ===================== START SERVER =====================
 
-app.listen(5000, () => {
-  console.log("🚀 Server Running on http://localhost:5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server Running on Port ${PORT}`);
 });
