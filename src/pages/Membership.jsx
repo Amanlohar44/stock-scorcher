9ea21e2b20b1a63aab84f1bc65799ffcffa32c67
Aman Lohar <loharaman44@gmail.com>
2026-07-24 18:36:22 +0530

    Fix Membership navbar import

--- src/pages/Membership.jsx
+++ src/pages/Membership.jsx
@@ -1,4 +1,4 @@
-import Navbar from "../components/NavbarOld";
+import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 
 import MembershipHero from "../components/membership/MembershipHero";
