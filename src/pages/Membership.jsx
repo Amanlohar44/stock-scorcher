81d640bbc72412d55763780acdd84ea6d173a5cf
Aman Lohar <loharaman44@gmail.com>
2026-07-24 19:35:42 +0530

    Recreate Membership page

--- src/pages/Membership.jsx
+++ src/pages/Membership.jsx
@@ -1,12 +1,11 @@
 import Navbar from "../components/navbar/Navbar";
 import Footer from "../components/Footer";
-
 import MembershipHero from "../components/membership/MembershipHero";
 import PricingCards from "../components/membership/PricingCards";
 
 export default function Membership() {
   return (
-    <div className="bg-black text-white min-h-screen">
+    <div className="min-h-screen bg-black text-white">
       <Navbar />
 
       <MembershipHero />
